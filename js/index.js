bb27ffab4c4fbf6a6e2453aebc1efdc39e857ef5
marwa-ahmed93 <https://github.com/marwa-ahmed93/Restaurant.git>
2024-10-19 22:22:12 +0300

    adding animation

--- js/index.js
+++ js/index.js
@@ -3,24 +3,24 @@ let aboutOffset = $("#about").offset().top;
 
 $(window).scroll(function () {
 
-    let wScroll = $(window).scrollTop();
+  let wScroll = $(window).scrollTop();
 
-    if (wScroll > aboutOffset -50) {
-        $("#mainNav").css("backgroundColor", "white");
-        // $("#navColor").css("color", "red");
-        $('nav li a').css('color', 'black');
-        $('#navLogo').attr('src', './images/bakery-color.png');
-     
-    }
-    else {
-        $("#mainNav").css("backgroundColor", "transparent");
-        $('nav li a').css('color', 'white');
-        $('#navLogo').attr('src', './images/bakery-light-1.png');
-       
-    }
-  });
+  if (wScroll > aboutOffset - 50) {
+    $("#mainNav").css("backgroundColor", "white");
+    // $("#navColor").css("color", "red");
+    $('nav li a').css('color', 'black');
+    $('#navLogo').attr('src', './images/bakery-color.png');
 
-$("#setingIcon").click(function(){
+  }
+  else {
+    $("#mainNav").css("backgroundColor", "transparent");
+    $('nav li a').css('color', 'white');
+    $('#navLogo').attr('src', './images/bakery-light-1.png');
+
+  }
+});
+
+$("#setingIcon").click(function () {
   $('.iconSetDiv').toggle();
 
   // Toggle color between blue and black
@@ -32,13 +32,13 @@ $("#setingIcon").click(function(){
   }
 });
 
-$("#carIcon").click(function(){
+$("#carIcon").click(function () {
   $('.icontDariv').toggle();
 
-  $("#carIcon").css("color", "blue");  
+  $("#carIcon").css("color", "blue");
 });
 
-$(".btnIcon").click(function(){
+$(".btnIcon").click(function () {
   $("html,body").animate({ scrollTop: 0 }, 500)
 });
 
